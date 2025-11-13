64965f73211748efaaccf40f78ace3d366ab3300
caua <cauacouto022@gmail.com>
2025-11-12 22:58:21 -0300

    primeiro commit

---
+++ src/main/java/com/coutodev/psoto_combustivel/PsotoCombustivelApplication.java
@@ -0,0 +1,13 @@
+package com.coutodev.psoto_combustivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsotoCombustivelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsotoCombustivelApplication.class, args);
+	}
+
+}
